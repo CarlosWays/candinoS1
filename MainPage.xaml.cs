17d8caae52879e866dd2ff99e8ab9db4d79139e8
Carlos <Carlos@CARLOSPC>
2024-04-05 18:02:49 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace candinoS1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private string strNombre, strApellido;
+    private int intEdad;
+    private float flSalario, decTotalCalculado;
+
+
+    private void btnSaludoClicked(object sender, EventArgs e)
+    {
+        strNombre = txtNombre.Text;
+        strApellido = txtApellido.Text;
+        intEdad = int.Parse(txtEdad.Text);
+        flSalario = float.Parse(txtSalario.Text);
+        decTotalCalculado = (flSalario * 9.45f) / 100;
+        DisplayAlert("Resultados", "Bienvenido, " + strNombre + " " + strApellido + "\nTienes " + intEdad + " años" + 
+            "\nTu aporte mensual es " + decTotalCalculado, "Salir");
+        LimpiarDatos();
+    }
+
+    private void LimpiarDatos()
+    {
+        txtNombre.Text = string.Empty;
+        txtApellido.Text = string.Empty;
+        txtSalario.Text = string.Empty;
+        txtEdad.Text = string.Empty;
+
+    }
+}
+
